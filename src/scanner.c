065a0f944ad77e62813950c6162d7d14f3284c94
Khanh <ngokhanh809@gmail.com>
2023-08-23 11:36:32 -0700

    end of tokenizer chapter

--- src/scanner.c
+++ src/scanner.c
@@ -71,13 +71,45 @@ static char peek_next(const Scanner_t* scanner, int offset);
 static Token_t string_token(Scanner_t* scanner);
 
 
-
+/*
+*   \returns a numnber literal token
+*/
 static Token_t number_token(Scanner_t* scanner);
 
 
+/*  
+*   \returns true if ch is a digit, false otherwise
+*/
+static bool is_digit(char ch);
+
+
+/*
+*   \returns true if ch is a alphabetical character or '_', false otherwise
+*/
+static bool is_alpha(char ch);
+
+
+/*
+*   \returns a token that is either some kind of keyword or an identifier
+*/
+static Token_t identifier_token(Scanner_t* scanner);
 
 
 
+/*
+*   \returns the type of the token we're at
+*/
+static TokenType_t chk_iden_type(Scanner_t* scanner);
+
+
+/*
+*   \returns the given type param if the current lexeme matches rest
+*   \returns else return TOKEN_IDENTIFIER
+*/
+static TokenType_t chk_keyword(const Scanner_t* scanner, 
+    int head_offset, int len, 
+    const char* rest, TokenType_t type
+);
 
 
 
@@ -109,6 +141,10 @@ Token_t Scanner_ScanToken(Scanner_t* scanner)
     }
 
     const char ch = advance(scanner);
+    if (is_alpha(ch))
+    {
+        return identifier_token(scanner);
+    }
     if (is_digit(ch))
     {
         return number_token(scanner);
@@ -128,7 +164,6 @@ Token_t Scanner_ScanToken(Scanner_t* scanner)
     case '+': return make_token(scanner, TOKEN_PLUS);
     case '/': return make_token(scanner, TOKEN_SLASH);
     case '*': return make_token(scanner, TOKEN_STAR);
-    case '"': return string_token(scanner);
 
     case '!':
         if (match(scanner, '='))
@@ -151,6 +186,7 @@ Token_t Scanner_ScanToken(Scanner_t* scanner)
         else
             return make_token(scanner, TOKEN_GREATER);
 
+    case '"': return string_token(scanner);
     }
     
     return error_token(scanner, "Unexpected token.");
@@ -282,10 +318,122 @@ static Token_t string_token(Scanner_t* scanner)
     }
 
     if (is_at_end(scanner))
+    {
         return error_token(scanner, "Unterminated string.");
+    }
     
     /* consumes '"' */
     advance(scanner);
     return make_token(scanner, TOKEN_STRING);
+}
+
+
+static Token_t number_token(Scanner_t* scanner)
+{
+    while (is_digit(PEEK(scanner)))
+    {
+        advance(scanner);
+    }
+    if ('.' != PEEK(scanner))
+        return make_token(scanner, TOKEN_NUMBER);
+
+    /* skips decimal separator */
+    advance(scanner);
+
+    /* eat the rest of the number */
+    while (is_digit(PEEK(scanner)))
+    {
+        advance(scanner);
+    }
+    return make_token(scanner, TOKEN_NUMBER);
+}
+
+
+static bool is_digit(char ch)
+{
+    return ('0' <= ch) && (ch <= '9');
+}
+
+
+
+static bool is_alpha(char ch)
+{
+    return (('a' <= ch) && (ch <= 'z'))
+        || (('A' <= ch) && (ch <= 'Z'))
+        || ('_' == ch);
+}
+
+
+
+
+static Token_t identifier_token(Scanner_t* scanner)
+{
+    /* consumes the identifier */
+    while (is_alpha(PEEK(scanner)) || is_digit(PEEK(scanner)))
+    {
+        advance(scanner);
+    }
+    return make_token(scanner, chk_iden_type(scanner));
+}
+
+
+
+
+static TokenType_t chk_iden_type(Scanner_t* scanner)
+{
+    switch (scanner->start[0]) 
+    {
+        case 'a': return chk_keyword(scanner, 1, 2, "nd", TOKEN_AND);
+        case 'c': return chk_keyword(scanner, 1, 4, "lass", TOKEN_CLASS);
+        case 'e': return chk_keyword(scanner, 1, 3, "lse", TOKEN_ELSE);
+        case 'i': return chk_keyword(scanner, 1, 1, "f", TOKEN_IF);
+        case 'n': return chk_keyword(scanner, 1, 2, "il", TOKEN_NIL);
+        case 'o': return chk_keyword(scanner, 1, 1, "r", TOKEN_OR);
+        case 'p': return chk_keyword(scanner, 1, 4, "rint", TOKEN_PRINT);
+        case 'r': return chk_keyword(scanner, 1, 5, "eturn", TOKEN_RETURN);
+        case 's': return chk_keyword(scanner, 1, 4, "uper", TOKEN_SUPER);
+        case 'v': return chk_keyword(scanner, 1, 2, "ar", TOKEN_VAR);
+        case 'w': return chk_keyword(scanner, 1, 4, "hile", TOKEN_WHILE);
+        case 'f':
+            if (scanner->curr - scanner->start > 1) /* not just 'f' */
+            {
+                switch (scanner->start[1]) 
+                {
+                    case 'a': return chk_keyword(scanner, 2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return chk_keyword(scanner, 2, 1, "r", TOKEN_FOR);
+                    case 'u': return chk_keyword(scanner, 2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 't':
+            if (scanner->curr - scanner->start > 1) /* not just 't' */
+            {
+                switch (scanner->start[1]) 
+                {
+                    case 'h': return chk_keyword(scanner, 2, 2, "is", TOKEN_THIS);
+                    case 'r': return chk_keyword(scanner, 2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+    }
+    return TOKEN_IDENTIFIER;
+}
+
+
+
+
+static TokenType_t chk_keyword(const Scanner_t* scanner,
+    int start, int len,
+    const char* rest, TokenType_t type
+)
+{
+    const size_t lexeme_len = scanner->curr - scanner->start;
+    const size_t keyword_len = start + strlen(rest);
+    if ((lexeme_len == keyword_len)
+    && memcmp(scanner->start + start, rest, len) == 0)
+    {
+        return type;
+    }
+    return TOKEN_IDENTIFIER;
 }
 
